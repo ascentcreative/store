21bf06ee97b2f0d7ed8f5391af9a465ab8ea4999
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-18 16:06:01 +0100

    Configurable store path

--- config/store.php
+++ config/store.php
@@ -2,5 +2,6 @@
 
 return [
 
+    'store_path' => 'store',
     
 ];
--- routes/store-web.php
+++ routes/store-web.php
@@ -2,33 +2,21 @@
 
 use AscentCreative\Store\Models\Product;
 
-Route::namespace('AscentCreative\Store\Controllers')->middleware(['web'])->group(function() {
+Route::namespace('AscentCreative\Store\Controllers')->middleware(['web'])->group(function() {   
 
-    // Route::get('store-test/{product}', function(Product $product) {
+    Route::prefix(config('store.store_path'))->group(function() {
 
-    //     basket()->add($product, 5);
+        Route::get('/', function() {
+            echo 'cat home';
+        });
 
-    // });
+        Route::basket('product', AscentCreative\Store\Models\Product::class);
 
-    Route::basket('product', AscentCreative\Store\Models\Product::class);
+        Route::resource('product', ProductController::class, ['as'=>'store'])
+                ->parameters([
+                    'product' => 'product:slug'
+                ]);
 
-
-
-
-    Route::resource('product', ProductController::class, ['as'=>'store'])
-            ->parameters([
-                'product' => 'product:slug'
-            ]);
-    
-
-
-    // Route::resource('/image', App\Http\Controllers\ImageController::class)->parameters([
-    //     'image' => 'image:slug'
-    // ]);
-    // function(Product $product) {
-    //     dd($product);
-    // })->name('store.products.show');
-
-
+    });
 
 });
