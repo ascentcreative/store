f6714dcdaf98c565f9ea20bc1174b3727a01b146
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-28 16:22:31 +0100

    Test stack editor integration

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -8,11 +8,12 @@
 
         {{-- <A href="{{ route('product.basket.add', ['sku'=>trim(json_encode([$model->sku, $model->sku]))]) }}">Test</a> --}}
         
-        <A href="{{ route('product.basket.add', ['sku'=>$model->sku]) }}">Test</a>
+        <A href="{{ route('product.basket.add', ['sku'=>$model->sku]) }}/size/large/colour/blue">Test</a>
 
-        <A href="{{ route('product.basket.add.qty', ['sku'=>$model->sku, 'qty'=>5]) }}">Test Buy 5</a>
+        <A href="{{ route('product.basket.add', ['sku'=>$model->sku, 'qty'=>5]) }}">Test Buy 5</a>
 
-            
+        @include('stackeditor::render', ['content' => $model->description])    
+        
 
     </div>
 
