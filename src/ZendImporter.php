144c6fe1ffbaaf3d63e5a5d932809d263baa9b6d
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-11-02 12:09:22 +0000

    Importer updates - images and class config

--- src/ZendImporter.php
+++ src/ZendImporter.php
@@ -4,27 +4,48 @@ namespace AscentCreative\Store;
 
 use Illuminate\Support\Facades\DB;
 
-use AscentCreative\Store\Models\Product;
+// use AscentCreative\Store\Models\Product;
+use AscentCreative\Images\Models\Image;
 
 class ZendImporter {
 
     static function import() {
 
-        Product::truncate();
+        // dd(app('product'));
+
+        app('product')::truncate();
+
+        Image::where('imageable_type', config('store.models.product'))->delete();
 
         $res = DB::connection('zend')->select('select * from store_product');        
 
         foreach($res as $prod) {
 
-            $product = Product::create([
+            $product = app('product')::create([
 
                 'title' => $prod->name,
                 'sku' => $prod->sku,
                 'description' => $prod->description,
                 'short_description' => $prod->summary,
                 'price' => $prod->priceGBP,
+                'is_physical' => $prod->isPhysical,
+                'is_download' => $prod->isDownload
 
             ]);
+
+            $image = basename($prod->imageFile);
+
+            $image = Image::create([
+                'imageable_type' => app('product')::class,
+                'imageable_id' => $product->id,
+                'imageable_key' => 'images',
+                'imageable_sort' => 0,
+                'original_filename' => $image,
+                'hashed_filename' => $image
+
+            ]);
+
+
 
         }
 
