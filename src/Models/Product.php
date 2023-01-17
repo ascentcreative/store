e1f21145ce67eb32ae654f2dcf0e272f827e5ede
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-01-17 15:08:50 +0000

    Indexable

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -20,13 +20,23 @@ use AscentCreative\Store\Traits\Stockable;
 use AscentCreative\Images\Traits\HasGalleries;
 use AscentCreative\Store\Traits\HasCategories;
 
+use AscentCreative\SiteSearch\Traits\Indexable;
+
 class Product extends Model implements Sellable
 {
-    use HasFactory, HasMetadata, HasSlug, Shippable, Stockable, SellableTrait, Autocompletable, HasGalleries, HasCategories, Publishable;
+    use HasFactory, HasMetadata, HasSlug, Shippable, Stockable, SellableTrait, Autocompletable, HasGalleries, HasCategories, Publishable, Indexable;
 
     public $table = 'store_products';
 
     public $fillable = ['id', 'title', 'description', 'short_description', 'sku', 'weight', 'price', 'is_physical', 'is_download', 'file_id'];
+
+
+    public function getIndexableAttribute() {
+        return ['title', function($model) {
+                return 'xyz';
+            }];
+    }
+
 
     public $autocomplete_search = ['title'];
 
